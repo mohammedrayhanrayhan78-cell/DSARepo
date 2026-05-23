273760b49802e091d1672050fe34ba76441fd3f2
F1 <mohammedrayhanrayhan78-cell>
2026-05-23 06:44:58 +0530

    day 7 Updated code

---
+++ DAY_7.java
@@ -0,0 +1,56 @@
+// import java.util.Scanner;
+
+// public class DAY_7 {
+//     class Solution {
+//     public void printNumber(Scanner sc) {
+//         System.out.println(sc.nextInt());
+//     }
+// }
+// }
+// public class DAY_7{
+// class Solution {
+//     public void studentGrade(int marks) {
+//     if(marks>=95){
+//         System.out.print("Grade A");
+//     }
+//     else if(marks>=70){
+//         System.out.print("Grade B");
+//     }
+//     else{
+//         System.out.print("Fail");
+//     }
+//     }
+// }
+// }
+// public class DAY_7{
+// class Solution {
+//     public void whichWeekDay(int day) {
+//     switch(day){
+//         case 1:
+//             System.out.print("Monday");
+//             break;
+//         case 2:
+//             System.out.print("Tuesday");
+//             break;
+//         case 3:
+//             System.out.print("Wednesday");
+//             break;
+//         case 4:
+//             System.out.print("Thursday");
+//             break;
+//         case 5:
+//             System.out.print("Friday");
+//             break;
+//         case 6:
+//             System.out.print("Saturday");
+//             break;
+//         case 7:
+//             System.out.print("Sunday");
+//             break;
+//         case 8:
+//             System.out.print("Invalid");
+//             break;
+//         }
+//     }
+//     }
+// }
