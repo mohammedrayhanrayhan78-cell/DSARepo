2a5aeceff0b017190f7cc101a2a9eb1ac886779f
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-22 18:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.47%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix=strs[0]
+
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix=prefix[:-1]
+                if prefix=="":
+                    return ""
+        return prefix
+strs = ["flower","flow","flight"]
+sol=Solution()
+print(sol.longestCommonPrefix(strs))
