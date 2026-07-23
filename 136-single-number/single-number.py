12ff37d5f1ee4649518749f8e30b555b370d4fa0
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-23 15:53:40 +0530

    Time: 9 ms (19.03%) | Memory: 21.8 MB (10.19%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen={}
+        for num in nums:
+            if num in seen:
+                seen[num]+=1
+            else:
+                seen[num]=1
+        for number, times_seen in seen.items():
+            if times_seen == 1:
+                return number
+
+nums = [2,2,1]
+sol=Solution()
+print(sol.singleNumber(nums))            
