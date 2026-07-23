c2664b4d4b32d71c6bfe364600027061e2135c64
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-23 22:41:58 +0530

    Delete 242-valid-anagram directory

--- 242-valid-anagram/valid-anagram.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        for char in s:
-            return sorted(s)==sorted(t)
-s = "anagram"
-t = "nagaram"
-sol=Solution()
-print(sol.isAnagram(s,t))
-
