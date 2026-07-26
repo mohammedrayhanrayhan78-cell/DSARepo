751be88ed6ac491bb7dd07ecd5dc19b78db4482c
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-26 12:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.37%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth=0
+        max_depth=0
+        for char in s:
+            if char == '(':
+                depth+=1
+                if depth>max_depth:
+                    max_depth=depth
+            elif char == ')':
+                depth-=1
+        return max_depth
+s = "(1)+((2))+(((3)))"
+sol=Solution()
+print(sol.maxDepth(s))
