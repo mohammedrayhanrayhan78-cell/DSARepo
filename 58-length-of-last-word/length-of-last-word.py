85594909174530715d5e339a31b8577ac4213528
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-26 07:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (98.12%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        last_word=words[-1]
+        return len(last_word)
+s = "Hello World"
+sol=Solution()
+print(sol.lengthOfLastWord(s))
