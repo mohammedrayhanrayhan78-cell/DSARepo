8792ab1c4375c19f37c8e2e6343b852cf962b16c
Mohammed Rayhan <mohammedrayhanrayhan78@gmail.com>
2026-07-29 07:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.82%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo={}
+        def helper(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            if n in memo:
+                return memo[n]
+            memo[n]=helper(n-1) + helper(n-2)
+            return memo[n]
+        return helper(n)
+n = 2
+sol=Solution()
+print(sol.climbStairs(n))
